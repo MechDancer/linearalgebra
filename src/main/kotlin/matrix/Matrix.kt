c9795027700a08d7fdcd8080593966591fea4655
berberman <1793913507@qq.com>
2018-04-09 21:31:20 +0800

    :pencil2: fix a spelling mistake of matrix

---
+++ src/main/kotlin/matrix/Matrix.kt
@@ -0,0 +1,39 @@
+package matrix
+
+import vector.Vector
+
+interface Matrix {
+
+	val dimension: Int
+
+	val defineType: DefineType
+
+	val data: List<List<Double>>
+
+	val row: Int
+
+	val column: Int
+
+	operator fun get(row: Int, column: Int): Double
+
+	operator fun plus(other: Matrix): Matrix
+
+	operator fun minus(other: Matrix): Matrix
+
+	operator fun times(k: Double): Matrix
+
+	operator fun times(other: Matrix): Matrix
+
+	operator fun times(vector: Vector): Vector
+
+	operator fun div(other: Matrix): Matrix
+
+
+	fun companion(): Matrix
+
+	fun transpose(): Matrix
+
+	fun inverse(): Matrix
+
+
+}
