dfe6b61dffaf412f54021aa27ac49812f3700ff4
YangDR <ydrml@hotmail.com>
2018-10-10 18:26:46 +0800

    添加最小二乘解超定方程
    Signed-off-by: YangDR <ydrml@hotmail.com>

--- src/main/kotlin/org/mechdancer/algebra/function/Equation.kt
+++ src/main/kotlin/org/mechdancer/algebra/function/Equation.kt
@@ -2,8 +2,7 @@ package org.mechdancer.algebra.function
 
 import org.mechdancer.algebra.core.Matrix
 import org.mechdancer.algebra.core.Vector
-import org.mechdancer.algebra.function.matrix.lastRow
-import org.mechdancer.algebra.function.matrix.simplifyAssignWith
+import org.mechdancer.algebra.function.matrix.*
 import org.mechdancer.algebra.function.vector.isNotZero
 import org.mechdancer.algebra.implement.matrix.builder.toArrayMatrix
 
@@ -14,9 +13,24 @@ import org.mechdancer.algebra.implement.matrix.builder.toArrayMatrix
  */
 @Suppress("NAME_SHADOWING")
 fun solve(arguments: Matrix, constant: Vector): Vector? {
-	assert(arguments.row == constant.dim)
+	assert(arguments.dim == constant.dim)
 	val arguments = arguments.toArrayMatrix()
 	val constant = constant.toArrayMatrix()
+	arguments.simplifyAssignWith(constant)
+	return constant.column(0).takeIf { arguments.lastRow.isNotZero() }
+}
+
+/**
+ * 最小二乘解超定方程
+ * @param arguments 系数矩阵
+ * @param constant  常数项
+ */
+@Suppress("NAME_SHADOWING")
+fun leastSquareSolve(arguments: Matrix, constant: Vector): Vector? {
+	assert(arguments.column == constant.dim)
+	assert(arguments.row > arguments.column)
+	val arguments = (arguments.transpose() * arguments).toArrayMatrix()
+	val constant = (arguments.transpose() * constant).toArrayMatrix()
 	arguments.simplifyAssignWith(constant)
 	return constant.column(0).takeIf { arguments.lastRow.isNotZero() }
 }
